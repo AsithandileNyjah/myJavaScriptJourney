f276760b01bb5301a0b1503ce0f9e684eabfd44a
AsithandileNyjah <atyuluu@gmail.com>
2023-11-08 10:27:34 +0000

    Variables2

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ let area = length*width
 
 console.log(`the are of the rectangle is ${area} where it was calculated`)
 
+// let num1 = 10
+// let num2 = 4
+// function remainder {
+//     console.log (`d`)
+// }
+let num1 = 10;
+let num2 = 4;
+
+let remainder = num1 % num2;
+
+console.log(`num1 / num2 = ${remainder}`);
