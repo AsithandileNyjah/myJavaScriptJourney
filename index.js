3357edc62849f7a3c65be10764e1005e514065dc
AsithandileNyjah <atyuluu@gmail.com>
2023-11-08 10:13:48 +0000

    Variables

---
+++ index.js
@@ -0,0 +1,10 @@
+let language = 'JavaScript';
+
+let score = 10;
+
+let length = 10;
+let width = 5;
+let area = length*width
+
+console.log(`the are of the rectangle is ${area} where it was calculated`)
+
